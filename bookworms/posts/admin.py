3cdc42c25f39a43463515cc271001200b9c161f1
glebmelnikov20@gmail.com <glebmelnikov20@gmail.com>
2026-03-22 10:50:21 +0100

    Remove duplicate post app

--- bookworms/posts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'created_at', 'updated_at')
-    list_filter = ('created_at', 'updated_at')
-    search_fields = ('author__username', 'content')
